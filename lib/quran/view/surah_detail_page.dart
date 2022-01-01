d2b15d3227a5b05b1f006e6f5a4cf4b29fe99ed0
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-01-01 23:37:27 +0700

    feat ✨: surah detail page

---
+++ lib/quran/view/surah_detail_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/quran/view/widget/surah_card.dart';
+import 'package:quran_app/utils/theme.dart';
+
+class SurahDetailPage extends StatelessWidget {
+  const SurahDetailPage(
+      {Key? key,
+      this.number,
+      this.nameTransliteration,
+      this.nameTranslation,
+      this.revelation,
+      this.nameShort,
+      this.numberOfVerses})
+      : super(key: key);
+  final int? number;
+  final String? nameTransliteration;
+  final String? nameTranslation;
+  final String? revelation;
+  final String? nameShort;
+  final int? numberOfVerses;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sahibul Quran"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SurahCard(
+              number: number,
+              nameShort: "$nameShort",
+              nameTranslation: "$nameTranslation",
+              nameTransliteration: "$nameTransliteration",
+              numberOfVerses: numberOfVerses,
+              revelation: "$revelation",
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
