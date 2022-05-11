5ca2676d6980f332ff27722cf27b72bd0c322b24
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-05-11 21:09:29 +0700

    fix: widget issue

--- lib/src/quran/view/surah_detail_page.dart
+++ lib/src/quran/view/surah_detail_page.dart
@@ -49,73 +49,61 @@ class SurahDetailPage extends StatelessWidget {
         centerTitle: true,
         elevation: 1,
       ),
-      body: ConnectivityWidgetWrapper(
-        decoration: BoxDecoration(
-          boxShadow: [AppShadow.card],
-          color: ColorPalletes.frenchPink.withOpacity(0.2),
-        ),
-        messageStyle: AppTextStyle.small.copyWith(
-          color: ColorPalletes.frenchPink,
-          fontWeight: FontWeight.w600,
-          letterSpacing: 0.5,
-        ),
-        alignment: Alignment.topCenter,
-        child: FutureBuilder(
-          future: controller.fetchSurahByID(number),
-          builder: (ctx, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const AppLoading();
-              // return const SurahDetailPageShimmer();
-            } else if (!snapshot.hasData) {
-              return const SurahDetailPageShimmer();
-            } else {
-              return Obx(() {
-                return Stack(
-                  children: [
-                    SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 20),
-                          SurahCard(
-                            number: number,
-                            nameShort: "$nameShort",
-                            nameTranslation: "$nameTranslation",
-                            nameTransliteration: "$nameTransliteration",
-                            numberOfVerses: numberOfVerses,
-                            revelation: "$revelation",
-                          ),
-                          // const SizedBox(height: 10),
-                          if (!snapshot.hasData) const SurahDetailPageShimmer(),
-                          for (var verse in controller.verses)
-                            FadeInDown(
-                              from: 50,
-                              child: VerseItem(
-                                numberInSurah: verse.number!.inSurah,
-                                textArab: verse.text!.arab,
-                                textTransliteration:
-                                    verse.text!.transliteration!.en,
-                                textTranslation: verse.translation!.id,
-                                onTapSeeTafsir: () {
-                                  if (controller.showTafsir.value) {
-                                    controller.showTafsir.value = false;
-                                  }
+      body: FutureBuilder(
+        future: controller.fetchSurahByID(number),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const AppLoading();
+            // return const SurahDetailPageShimmer();
+          } else if (!snapshot.hasData) {
+            return const SurahDetailPageShimmer();
+          } else {
+            return Obx(() {
+              return Stack(
+                children: [
+                  SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        SurahCard(
+                          number: number,
+                          nameShort: "$nameShort",
+                          nameTranslation: "$nameTranslation",
+                          nameTransliteration: "$nameTransliteration",
+                          numberOfVerses: numberOfVerses,
+                          revelation: "$revelation",
+                        ),
+                        // const SizedBox(height: 10),
+                        if (!snapshot.hasData) const SurahDetailPageShimmer(),
+                        for (var verse in controller.verses)
+                          FadeInDown(
+                            from: 50,
+                            child: VerseItem(
+                              numberInSurah: verse.number!.inSurah,
+                              textArab: verse.text!.arab,
+                              textTransliteration:
+                                  verse.text!.transliteration!.en,
+                              textTranslation: verse.translation!.id,
+                              onTapSeeTafsir: () {
+                                if (controller.showTafsir.value) {
+                                  controller.showTafsir.value = false;
+                                }
 
-                                  controller.showTafsir.value = true;
-                                  _buildTafsirView(verse);
-                                },
-                              ),
+                                controller.showTafsir.value = true;
+                                _buildTafsirView(verse);
+                              },
                             ),
-                          const SizedBox(height: 20),
-                        ],
-                      ),
+                          ),
+                        const SizedBox(height: 20),
+                      ],
                     ),
-                    if (controller.showTafsir.value) tafsirView,
-                  ],
-                );
-              });
-            }
-          },
-        ),
+                  ),
+                  if (controller.showTafsir.value) tafsirView,
+                ],
+              );
+            });
+          }
+        },
       ),
     );
   }
